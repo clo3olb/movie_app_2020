55139137e623921caa9873ca4bc595457610dc33
joseph <clo3olb@gmail.com>
2020-08-16 11:32:50 +0000

    Updates

---
+++ precache-manifest.397a7d15ed434bb2f75596770e396547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3185ffc177bb7489a4eeab56801a6e63",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "374302a2261dfb6fa7d3",
+    "url": "/movie_app_2020/static/css/main.7ca2bf17.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374302a2261dfb6fa7d3",
+    "url": "/movie_app_2020/static/js/main.3998ba0a.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
